136ceeb4dcba9d9dab3aaac88df25f43d104f21d
Raza <danmoleomomayowa@gmail.com>
2025-01-21 04:09:57 +0100

    Added data to project table database from sql terminal

--- Builders_Deck/Data/ApplicationDbContext.cs
+++ Builders_Deck/Data/ApplicationDbContext.cs
@@ -10,10 +10,6 @@ namespace Builders_Deck.Data
         {
             base.OnModelCreating(builder);
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            builder.Entity<Project>().HasData(
-        new Project { Id = 1, Title = "Luxury Villa", Description = "Completed by XYZ Constructions", ImageUrl = "/images/villa.jpg", CompletionDate = DateTime.Now.AddDays(-30), ContractorName = "XYZ Constructions" },
-        new Project { Id = 2, Title = "Modern Office", Description = "Completed by ABC Builders", ImageUrl = "/images/office.jpg", CompletionDate = DateTime.Now.AddDays(-60), ContractorName = "ABC Builders" }
-    );
         }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Contractor> Contractors { get; set; }
